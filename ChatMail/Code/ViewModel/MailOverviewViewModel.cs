6759bcef6ec171e71739f4ba541d27f7ab1623f8
ins1d3 <andjus.github@outlook.de>
2014-11-21 14:21:22 +0100

    Edit on Views

--- ChatMail/Code/ViewModel/MailOverviewViewModel.cs
+++ ChatMail/Code/ViewModel/MailOverviewViewModel.cs
@@ -2,23 +2,68 @@
 using ChatMail.Code.ViewModelService;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ChatMail.Code.ViewModel
 {
     public class MailOverviewViewModel : BaseModelService
     {
-        private List<Message> messages;
+        #region Binding Fields
 
-        public List<Message> Messages
+        private string responseMessageText;
+
+        public string ResponseMessageText
         {
-            get { return messages; }
-            set { messages = value;
-                RaisePropertyChanged("Messages");
+            get { return responseMessageText; }
+            set { responseMessageText = value;
+                RaisePropertyChanged("ResponseMessageText");
             }
         }
 
+        private int messageListSelectedIndex;
+
+        public int MessageListSelectedIndex
+        {
+            get { return messageListSelectedIndex; }
+            set
+            {
+                messageListSelectedIndex = value;
+                SelectedMessage = Messages[value];
+                RaisePropertyChanged("MessageListSelectedIndex");
+            }
+        }
+
+        private Message selectedMessage;
+
+        public Message SelectedMessage
+        {
+            get { return selectedMessage; }
+            set
+            {
+                selectedMessage = value;
+                RaisePropertyChanged("SelectedMessage");
+            }
+        }
+
+        #endregion
+
+        private ObservableCollection<Message> messages;
+        public ObservableCollection<Message> Messages
+        {
+            get { return messages; }
+            set { messages = value;
+            }
+        }
+
+
+
+        public void SendResponse()
+        {
+            
+        }
     }
 }
