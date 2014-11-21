c6bb6dfb1a7231768a27ca0205d8b752e50f66b2
ins1d3 <andjus.github@outlook.de>
2014-11-21 12:01:07 +0100

    missing files

---
+++ ChatMail/Code/ViewModel/MailOverviewViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ChatMail.Code.Models;
+using ChatMail.Code.ViewModelService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatMail.Code.ViewModel
+{
+    public class MailOverviewViewModel : BaseModelService
+    {
+        private Message message;
+
+        public Message Message
+        {
+            get { return message; }
+            set { message = value;
+                RaisePropertyChanged("Message");
+            }
+        }
+
+    }
+}
---
+++ ChatMail/Code/ViewModelService/ContentControlService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ChatMail.Code.ViewModelService
+{
+    class ContentControlService : BaseModelService
+    {
+        private int showedWindow;
+
+        public int ShowedWindow
+        {
+            get { return showedWindow; }
+            set { showedWindow = value;
+                RaisePropertyChanged("ShowedWindow");
+            }
+        }
+    }
+}
